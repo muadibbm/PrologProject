babafa50ba5454d42c8619cd9a3425972e49cd2e
Rasel Chowdhury <raselc@gmail.com>
2015-02-12 18:48:59 -0500

    update in main.pl
    simple test cases

--- Logic Dots/main.pl
+++ Logic Dots/main.pl
@@ -4,3 +4,11 @@
 
 run(RowConstraints, ColConstraints, BlockedCells) :- 
 	make_grid(RowConstraints, ColConstraints, BlockedCells, Grid). 
+	
+%this is the sample grid, we use it like problem(1, Rows), solve(Rows), maplist(writeln, Rows). in the terminal
+problem(1, [1,2,3],[1,2,3],[[_,_,_],
+							[_,_,_],
+							[_,_,_]
+							]).
+problem(2,[0,1],[0,1],[[_,_],
+					   [_,_]]).		
