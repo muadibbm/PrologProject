0eb4bf0ad3cb271568367d6d4d25d9e7a23444fd
Rasel Chowdhury <raselc@gmail.com>
2015-02-13 14:33:22 -0500

    added few predicates
    list adder adds number in list
    equally checks two list have same summation
    increment increases value by 1
    decrement decreases value by 1

--- Logic Dots/solve.pl
+++ Logic Dots/solve.pl
@@ -1,2 +1,16 @@
 
 solve([RowNum | Row], [ColNum | Col], [GridRow | RestOfGridRows], SolvedGrid) :- nl.
+
+
+
+list_adder([ ],0).
+list_adder([X|L],Sum) :- list_adder(L,SL), Sum is X + SL.
+
+equally(A,B):-
+	equals(list_adder(A,0),list_adder(B,0)).
+
+increment(A,B):-
+	B is A+1.
+	
+decrement(A,B):-
+	B is A-1.
