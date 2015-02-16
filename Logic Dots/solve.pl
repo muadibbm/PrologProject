18957499d5de1ac96125bcafa05024b979787bf7
Rasel Chowdhury <raselc@gmail.com>
2015-02-15 19:10:33 -0500

    update
    clean version

--- Logic Dots/solve.pl
+++ Logic Dots/solve.pl
@@ -1,30 +1,36 @@
 
 solve([RowNum | Row], [ColNum | Col], [GridRow | RestOfGridRows], SolvedGrid) :- nl.
 
+solve(Row,Column,Node,Path):-
+	equally(Row,Column),
+	list_adder(Row,Total),
+	breadthfirst(Row,Column,Total,[[Node]],RevPath),
+	reverse(RevPath,Path).
+
+breadthfirst(Row,Column,Total,[[Node|Path]|_],[Node|Path]):-
+	goal(Row,Column,Total,Node).
 
 
-list_adder([ ],0).
-list_adder([X|L],Sum) :- list_adder(L,SL), Sum is X + SL.
 
-equally(A,B):-
-	equals(list_adder(A,0),list_adder(B,0)).
-
-increment(A,B):-
-	B is A+1.
-	
-decrement(A,B):-
-	B is A-1.
-
-occurrences([],_,0).
-occurrences([X|Y],X,N):- occurrences(Y,X,W),N is W + 1.
-occurrences([X|Y],Z,N):- occurrences(Y,Z,N),X\=Z.
-
-goally(Row,Column,Total,Grid):- 
+goal(Row,Column,Total,Grid):- 
 	checkSumEquality(Row,0),
 	checkSumEquality(Column,0),
 	flatten(Grid,G),
 	checkTotal(G,Total).
-	
+
 checkSumEquality(L, S) :- Sum = S, list_adder(L, Sum).
 
-checkTotal(G,T):- Occ = T,occurrences(G,o,Occ).
+checkTotal(G,T):- Occ=T,occurrences(G,o,Occ).
+
+list_adder([],0).
+list_adder([X|L],Sum) :- list_adder(L,SL), Sum is X + SL.
+
+occurrences([],_,0).
+occurrences([X|Y],X,N):- occurrences(Y,X,W),N is W + 1.
+occurrences([X|Y],Z,N):- occurrences(Y,Z,N),X\=Z.	
+
+equally(A,B):-
+	equals(list_adder(A,_),list_adder(B,_)).
+
+decrement(A,B):-
+	B is A-1.
